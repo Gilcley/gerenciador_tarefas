0abc3361e167021b088ea4d54dcc916c4e32d0f5
Gilcley Silva <gilcleystaff@gmail.com>
2021-11-21 14:34:38 -0300

    script do banco

---
+++ CARGA.sql
@@ -0,0 +1,7 @@
+INSERT INTO prioridade (id, descricao) VALUES(1, 'BAIXA'),	(2, 'MÉDIA'),(3,'ALTA');
+
+INSERT INTO situacao (id, descricao) VALUES(1, 'EM ANDAMENTO'),	(2, 'CONCLUÍDA');
+
+INSERT INTO pessoa (id, data_nascimento , nome ) 
+	VALUES(1, '10/10/2001', 'JOSÉ NINGUÉM DOS SANTOS'),	
+	(2, '10/10/2001', 'MARIA OUTRA PESSOA');
